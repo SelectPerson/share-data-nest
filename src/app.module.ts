91d19f1aaec54645a4ab36748dec3b54cccfe17c
SelectPerson <vovalutsk961@gmail.com>
2022-11-15 16:10:46 +0200

    added crud system user with pg, sq and make jwt auth ( only login )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './api/users/users.module';
+import { AuthModule } from './api/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ProfileController } from './api/profile/profile.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'blog',
+      models: [],
+      autoLoadModels: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [ProfileController],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
---
+++ src/types/ProfileObject.ts
@@ -0,0 +1,5 @@
+export interface ProfileObject {
+  name: string;
+  email: string;
+  password: string;
+}
